6756708d39ace02dc7e8423b249661baf2f7a3a5
Rahul5758 <rahulthapliyal5758@gmail.com>
2019-10-23 22:50:03 +0530

    Added Comments

--- pyramid1.c
+++ pyramid1.c
@@ -1,18 +1,25 @@
+//Include header file for I/O
 #include<stdio.h>
-int main(){
+int main(){	
+	//Declare variables
 	int n,i,j,k;
+	//Take input number of rows
 	printf("Enter the number of rows");
 	scanf("%d", &n);
+	//Run loop till number of rows
 	for(i=1;i<=n;i++)
 	{
+		//print spaces
 		for(j=1;j<=(n-i);j++)
 		{
 			printf(" ");
 		}
+		//print stars of pyramid
 		for(k=1;k<=(2*i-1);k++)
 		{
 			printf("*");
 		}
+		//Move to next line
 		printf("\n");
 	}
 	return 0;
